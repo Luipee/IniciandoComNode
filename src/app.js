afffe0f0d8a642f2119ff7229511de4f4befc271
Luipee <lipeffcampos@gmail.com>
2023-01-16 14:29:40 -0300

    projeto finalizado!

--- src/app.js
+++ src/app.js
@@ -7,7 +7,15 @@ const { response } = require("express")
 const app = express()
 
 app.use (express.json())
-const products = []
+let products = []
+
+fs.readFile("products.json", "utf-8",  (err, data ) => {
+  if (err) {
+    console.log(err)
+} else {
+  products = JSON.parse(data)
+ }
+})
 
 app.post("/products" , (req , res) =>{
   const {name , price} = req.body
@@ -43,22 +51,37 @@ app.put("/products/:id", (req,res) => {
   const{id} = req.params
   const { name, price } = req.body
 
-  const productIndex = products.findIndex(product => product.id ===id)
-  products(productIndex) = {
+  const productIndex = products.findIndex((product) => product.id === id)
+  products[productIndex] = {
     ...products[productIndex],
     name,
     price,
   }
+  productFile()
+
   return res.json({message:"Produto alterado com sucesso"})
+
 })
 app.delete("/products/:id", (req, res) => {
   const{ id } = req.paragrams
   const productIndex = products.findIndex((product) =>
   product.id === id) 
   products.splice(productIndex, 1)
+  productFile()
   return res.json({message:"product"})
+  
 
   }
-)
+  )
+  function productFile(){
+    fs.writeFile("products.json" , JSON.stringify(products), (err) =>{
+      if(err){
+        console.log(err)
+      } else {
+        console.log("Produto inserido")
+      }
+  }
+)}
+
 app.listen( 4002, () => console.log("Server is running in port 4002"))
 
